9b4be4b235bb4c96932eb9295ddd375a84e9e8a6
z.zielinska <z.zielinska@insys.pl>
2021-04-22 23:38:56 +0200

    added chart, table and routing which not working :(

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,15 +1,38 @@
-$(document).ready(()=>{
-    confirm('Potwierdzam wystarczające upojenie alkoholem, aby rozpocząć ten projekt');
+// niedzialajaca nawiga z https://embed.plnkr.co/plunk/PBHp2Yli0BrfUm9h
+(function($) {
+  var routes = {},
+    defaultRoute = 'pages';
 
-    $('button').click(()=>{
-        $.ajax({
-            url: ":api/getData",
-            success: (result)=>{
-                $('#json').text(result.data);     
-            },
-            error: (result)=>{
-                console.log(result); 
-            }
-          });
-    })
-})
+  routes['pages'] = {
+    url: '/',
+    templateUrl: 'public/templates/index.html',
+  };
+
+  routes['page2'] = {
+    url: 'page2',
+    templateUrl: 'public/templates/page2.html',
+  };
+
+  $.router.setData(routes).setDefault(defaultRoute);
+
+  $.when($.ready).then(function() {
+    $.router.run('.my-view', 'pages');
+  });
+
+})(jQuery);
+
+// $(document).ready(()=>{
+//     confirm('Potwierdzam wystarczające upojenie alkoholem, aby rozpocząć ten projekt');
+
+//     $('button').click(()=>{
+//         $.ajax({
+//             url: ":api/getData",
+//             success: (result)=>{
+//                 $('#json').text(result.data);     
+//             },
+//             error: (result)=>{
+//                 console.log(result); 
+//             }
+//           });
+//     })
+// })
